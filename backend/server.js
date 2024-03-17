3ff347dcf6437c64079267b88ad33a3ab72fb31d
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-17 22:02:59 +0000

    Feat:Add Routes

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,54 @@ mongoose
   .catch((error) => {
     console.log("Unable to connect to Server and/or MongoDB", error);
   });
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+
+// REGISTER
+//POST REGISTER
+app.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, username, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error signing up" });
+  }
+});
+
+// GET REGISTERED USERS
+app.get("/register", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to get users" });
+  }
+});
+
+//GET LOGIN
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY, {
+      expiresIn: "1hr",
+    });
+    res.json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in" });
+  }
+});
